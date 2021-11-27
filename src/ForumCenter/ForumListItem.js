806bb8a5effddaba26160e68ef972bcbd639c511
Wantting <huangwanting068@gmail.com>
2021-11-27 17:56:35 -0500

    deit delete button

--- src/ForumCenter/ForumListItem.js
+++ src/ForumCenter/ForumListItem.js
@@ -1,16 +1,14 @@
-import { useState } from "react";
-
 const FromListItem = (props) => {
     let forumList = props.forumList;
     let handleForumdelete = props.handleForumdelete;
 
-    return ( 
+    return (
         <section id="forum-list">
-            {forumList.map((frm)=>(
+            {forumList.map((frm) => (
                 <section className="list-single-item" key={frm.id}>
                     <section className="item-row-info">
                         <label htmlFor="">add icons {frm.id}</label>
-                        <input type="text" value = {frm["text"]} />
+                        <input type="text" value={frm["text"]} />
                     </section>
 
                     <section className="item-row-info">
@@ -18,14 +16,16 @@ const FromListItem = (props) => {
                         <label htmlFor="">{frm["date"]}</label>
                         <label htmlFor="">Likes: {frm["likes"]}</label>
                         <button className="forum-delete"
-                        onClick={()=>{handleForumdelete(frm.id)}}>
+                            onClick={() => {
+                                handleForumdelete(frm.id);
+                            }}>
                             delete
                         </button>
                     </section>
-                </section> 
+                </section>
             ))}
         </section>
-     );
+    );
 }
- 
+
 export default FromListItem;
