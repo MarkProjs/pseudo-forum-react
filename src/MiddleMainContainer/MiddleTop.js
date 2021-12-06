3835dd7dafe4d48f859ad4ec90f2fc3c00da1bcf
Maedeh Hassani <maedeh.hassani@dawsoncollege.qc.ca>
2021-12-06 14:26:52 -0500

    nothing

--- src/MiddleMainContainer/MiddleTop.js
+++ src/MiddleMainContainer/MiddleTop.js
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from 'react';
-
+import React from "react";
 
 const MiddleTop = (props) => {
 	let forumData = props.forumData;
+	console.log(forumData);
 	let dropdown_row = document.getElementsByClassName("dropdown-row");
-	let [post, setPost]=useState(null);
-	
-	useEffect(() => {
-		addBaseOptions(forumData);
-	});
-
-
-	function addBaseOptions(forumData) {
+	// add options to Base Currency
+	function addBaseOptions(options) {
 		let oldcategory = document.getElementById("category");
 		if (oldcategory) {
 			oldcategory.remove();
@@ -22,147 +16,68 @@ const MiddleTop = (props) => {
 		let selectoption = document.createElement('option');
 		selectoption.innerText = "Select a category";
 		category.appendChild(selectoption);
-<<<<<<< HEAD
-		
+
 		Object.keys(options).forEach((key) => {
-=======
-		forumData.map((elem) => {
->>>>>>> d2829ad0ccb4d61d3fb2c17a5d0bf5e2be2ddecc
 			let childNode = document.createElement('option');
-			childNode.setAttribute("value", `${elem.name}`);
-			childNode.innerText = `${elem.name}`;
+			childNode.setAttribute("value", `${options[key].name}`);
+			childNode.innerText = `${options[key].name}`;
 			category.appendChild(childNode);
 		});
-		category.addEventListener('click', () => {
-			addToOptions(forumData);
+		category.addEventListener('click', ()=>{
+			addToOptions(options);
 		});
 	}
-
-	function addToOptions(forumData) {
+	// add options to To topics
+	function addToOptions(options) {
 		let oldtopic = document.getElementById("topic");
 		if (oldtopic) {
 			oldtopic.remove();
 		}
 		let categoryValue = document.getElementById("category").value;
-		let options = forumData.find((elem) => {
-			return elem.name === categoryValue;
+		let tempCat = null;
+		let objTopic = options.find((cat) => {
+			if (cat.name === categoryValue) {
+				tempCat = cat;
+			}
+			return tempCat;
 		});
-
+	
 		let topic = document.createElement("select");
 		topic.id = "topic";
 		dropdown_row[1].appendChild(topic);
-		let selectoption = document.createElement("option");
+		let selectoption = document.createElement('option');
 		selectoption.innerText = "Select a topic";
-		topic.appendChild(selectoption);
-		if (categoryValue === "Select a category") {
-			//nothing to do
-		} else {
-			options["topicList"].map((tpc) => {
-				let optionTopic = document.createElement('option');
-				optionTopic.setAttribute("value", `${tpc.topic_title}`);
-				optionTopic.innerText = `${tpc.topic_title}`;
-				topic.appendChild(optionTopic);
-				topic.addEventListener('click', () => {
-					showPost(forumData);
-				});
-			});
-		}
+		objTopic.topicList.forEach((tpc) => {
+			let optionTopic = document.createElement('option');
+			console.log(tpc);
+			optionTopic.setAttribute("value", `${tpc.topic_title}`);
+			optionTopic.innerText = `${tpc.topic_title}`;
+			topic.appendChild(optionTopic);
+		});
 	}
-
-// 	function showPost(tpc) {
-// 		let topic = document.getElementById("topic");
-// 		let newPost =[];
-		
-// 		Object.keys(tpc).find((elem)=>{
-// 			if(tpc.topic_title===topic.value){
-// 				newPost = tpc["listPosts"];
-// 				return  tpc["listPosts"];
-// 			}
-// 		});
-// 		newPost.map((elem)=>{
-// 			console.log(newPost[elem],"lllllll");
-// 			// let section = document.createElement('section');
-// 			// forumlist.appendChild(section);
-// 			// section.innerHTML=`${
-// 			// 	<button>${elem.id} </button>
-// 			// }`
-// 		});
-// 	}
-useEffect(()=>[
-	setPost(post)
-],[post]);
-function createElem(newPost){
-	// console.log(newPost,"hhhhh");
-	let forumlist = document.getElementById("forum-list");
 	
-    Object.keys(newPost).map((elem)=>{
-		console.log(newPost[elem]);
-		let section = document.createElement("section");
-		forumlist.appendChild(section);
-		forumlist.innerHTML=`<section className="list-single-item" key=>
-					<section className="item-row-info">
-                        <label htmlFor="">add icons </label>
-                        <input type="text" value={} />
-                        <img src="./like-button.png" id="like" alt="like" onClick={() => {
-                           
-
-                        }} />
-                        <img src="./dislike-icon-5.png" id="dislike"
-                            onClick={() => {
-                              
-                            }} alt="dislike" />
-                    </section>
-		</section>`;
-	})
-}
-function showPost(forumData) {
-        let topic = document.getElementById("topic").value;
-        let category = document.getElementById("category").value;
-        let newPost = {};
-            Object.keys(forumData).find((elem) => {
-                if (forumData[elem].name === category) {
-                    // console.log("yes",options[elem]);
-                    newPost = forumData[elem];
-                    return forumData[elem];
-                }
-            });
-
-			console.log(newPost);
-    //  let nesListPost = {};
-
-    //         Object.keys(newPost).find((elem) => {
-    //             if (newPost.topicList.topic_title === topic) {
-    //                 newPost = newPost.topicList.topic_title;
-    //                 return newPost[elem];
-    //             }
-    //         });
-
-    setPost(newPost);
-        //    console.log(newPost,"=========");
-		//    let topicTag = document.getElementById("topic");
-		//    topicTag.addEventListener('click', () => {
-			createElem(newPost);
-		// });
-		console.log(post);
-    }
-
 	return (
 		<section id="middle-top">
+
 			<section class="dropdown-row">
 				<label htmlFor="category">Categories</label>
-				<select name="category" id="category">
+				<select name="category" id="category" onMouseEnter={() => {
+					addBaseOptions(forumData);
+				}}>
 					<option value="">Select a category</option>
+
 				</select>
 			</section>
 			<section class="dropdown-row">
 				<label htmlFor="topic">Related Topics</label>
-				<select name="topic" id="topic" onClick={()=>{
-					// showPost(forumData);
-				}}>
+				<select name="topic" id="topic">
 					<option value="">Select a topic</option>
+
 				</select>
 			</section>
+
 		</section>
 	);
 }
+
 export default MiddleTop;
