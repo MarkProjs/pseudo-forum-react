2837bb5bd25d6a87ef79179dd6fabe5a90ca1c47
Mark Benedict Agluba <mark.agluba@dawsoncollege.qc.ca>
2021-12-06 13:53:42 -0500

    did some function mods

--- src/MiddleMainContainer/MiddleTop.js
+++ src/MiddleMainContainer/MiddleTop.js
@@ -13,18 +13,13 @@ const MiddleTop = (props) => {
 		let category = document.createElement("select");
 		category.id = "category";
 		dropdown_row[0].appendChild(category);
-		let selectoption = document.createElement('option');
-		selectoption.innerText = "Select a category";
-		category.appendChild(selectoption);
 		Object.keys(options).forEach((key) => {
 			let childNode = document.createElement('option');
 			childNode.setAttribute("value", `${options[key].name}`);
 			childNode.innerText = `${options[key].name}`;
 			category.appendChild(childNode);
 		});
-		category.addEventListener('click', ()=>{
-			addToOptions(options);
-		});
+		addToOptions(options);
 	}
 	// add options to To Currency
 	function addToOptions(options) {
@@ -34,16 +29,16 @@ const MiddleTop = (props) => {
 		}
 		let categoryValue = document.getElementById("category").value;
 		let objTopic = options.find((cat) => {
-			if (cat.name == categoryValue) {
-				return cat;
+			let tempCat = null;
+			if (cat.name === categoryValue) {
+				tempCat = cat;
 			}
+			return tempCat;
 		});
 	
 		let topic = document.createElement("select");
 		topic.id = "topic";
 		dropdown_row[1].appendChild(topic);
-		let selectoption = document.createElement('option');
-		selectoption.innerText = "Select a topic";
 		objTopic.topicList.forEach((tpc) => {
 			let optionTopic = document.createElement('option');
 			console.log(tpc);
@@ -58,7 +53,7 @@ const MiddleTop = (props) => {
 
 			<section class="dropdown-row">
 				<label htmlFor="category">Categories</label>
-				<select name="category" id="category" onMouseEnter={() => {
+				<select name="category" id="category" onClick={() => {
 					addBaseOptions(forumData);
 				}}>
 					<option value="">Select a category</option>
