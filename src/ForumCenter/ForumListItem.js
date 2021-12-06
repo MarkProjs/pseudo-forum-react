b16821e305bdf9d2627e675ef3915ac87b7e72bb
Wantting <huangwanting068@gmail.com>
2021-12-06 01:34:24 -0500

    change maintop value and fix bug

--- src/ForumCenter/ForumListItem.js
+++ src/ForumCenter/ForumListItem.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const FromListItem = (props) => {
     let forumList = props.forumList;
@@ -6,6 +7,40 @@ const FromListItem = (props) => {
     let handleForumLike = props.handleForumLike;
     let handleForumdislike = props.handleForumdislike;
     let forumData = props.forumData;
+
+   
+    let [post, setPost] = useState(null);
+    useEffect(()=>{
+
+    });
+
+    function showPost(){
+        
+    }
+    // const showPost = (topic, category) => {
+    //     setPost((post) => {
+    //         // let topic = document.getElementById("topic").value;
+    //         // let category = document.getElementById("category").value;
+    //         let newOptionsArray = {};
+    //         Object.keys(forumData).find((elem) => {
+    //             if (forumData[elem].name === category) {
+    //                 // console.log("yes",options[elem]);
+    //                 newOptionsArray = forumData[elem];
+    //                 return forumData[elem];
+    //             }
+    //         });
+    //         Object.keys(newOptionsArray).find((elem) => {
+    //             if (newOptionsArray[elem].topicList === topic) {
+    //                 // console.log("yes",options[elem]);
+    //                 newOptionsArray = newOptionsArray[elem];
+    //                 return newOptionsArray[elem];
+    //             }
+    //         });
+    //         post = newOptionsArray;
+    //        return post;
+    //     });
+    // }
+   
     return (
         <section id="forum-list">
             {forumList.map((frm, index) => (
@@ -15,10 +50,11 @@ const FromListItem = (props) => {
                         <input type="text" value={frm["text"]} />
                         <img src="./like-button.png" id="like" alt="like" onClick={() => {
                             handleForumLike(frm, index);
+                            
                         }} />
                         <img src="./dislike-icon-5.png" id="dislike"
                             onClick={() => {
-                              setTimeout(handleForumdislike(frm, index), 100);
+                                setTimeout(handleForumdislike(frm, index), 100);
                             }} alt="dislike" />
                     </section>
 
--- src/MiddleMainContainer/MiddleTop.js
+++ src/MiddleMainContainer/MiddleTop.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect} from "react";
+
 
 const MiddleTop = (props) => {
 	let forumData = props.forumData;
-	console.log(forumData);
 	let dropdown_row = document.getElementsByClassName("dropdown-row");
-	// add options to Base Currency
-	function addBaseOptions(options) {
+
+
+	useEffect(() => {
+		addBaseOptions(forumData);
+
+	}, [forumData]);
+
+
+	function addBaseOptions(forumData) {
 		let oldcategory = document.getElementById("category");
 		if (oldcategory) {
 			oldcategory.remove();
@@ -16,65 +23,63 @@ const MiddleTop = (props) => {
 		let selectoption = document.createElement('option');
 		selectoption.innerText = "Select a category";
 		category.appendChild(selectoption);
-		Object.keys(options).forEach((key) => {
+		forumData.map((elem) => {
 			let childNode = document.createElement('option');
-			childNode.setAttribute("value", `${options[key].name}`);
-			childNode.innerText = `${options[key].name}`;
+			childNode.setAttribute("value", `${elem.name}`);
+			childNode.innerText = `${elem.name}`;
 			category.appendChild(childNode);
 		});
-		category.addEventListener('click', ()=>{
-			addToOptions(options);
+		category.addEventListener('click', () => {
+			addToOptions(forumData);
 		});
 	}
-	// add options to To Currency
-	function addToOptions(options) {
+
+	function addToOptions(forumData) {
 		let oldtopic = document.getElementById("topic");
 		if (oldtopic) {
 			oldtopic.remove();
 		}
 		let categoryValue = document.getElementById("category").value;
-		let objTopic = options.find((cat) => {
-			if (cat.name == categoryValue) {
-				return cat;
-			}
+		let options = forumData.find((elem) => {
+			return elem.name === categoryValue;
 		});
-	
+
 		let topic = document.createElement("select");
 		topic.id = "topic";
 		dropdown_row[1].appendChild(topic);
-		let selectoption = document.createElement('option');
+		let selectoption = document.createElement("option");
 		selectoption.innerText = "Select a topic";
-		objTopic.topicList.forEach((tpc) => {
-			let optionTopic = document.createElement('option');
-			console.log(tpc);
-			optionTopic.setAttribute("value", `${tpc.topic_title}`);
-			optionTopic.innerText = `${tpc.topic_title}`;
-			topic.appendChild(optionTopic);
-		});
+		topic.appendChild(selectoption);
+		if (categoryValue === "Select a category") {
+			//nothing to do
+		} else {
+			options["topicList"].map((tpc) => {
+				let optionTopic = document.createElement('option');
+				// console.log(tpc);
+				optionTopic.setAttribute("value", `${tpc.topic_title}`);
+				optionTopic.innerText = `${tpc.topic_title}`;
+				topic.appendChild(optionTopic);
+			});
+		}
 	}
-	
+
+
+
 	return (
 		<section id="middle-top">
-
 			<section class="dropdown-row">
 				<label htmlFor="category">Categories</label>
-				<select name="category" id="category" onMouseEnter={() => {
-					addBaseOptions(forumData);
-				}}>
+				<select name="category" id="category">
 					<option value="">Select a category</option>
-
 				</select>
 			</section>
 			<section class="dropdown-row">
 				<label htmlFor="topic">Related Topics</label>
 				<select name="topic" id="topic">
 					<option value="">Select a topic</option>
-
 				</select>
 			</section>
-
 		</section>
 	);
 }
-
 export default MiddleTop;
