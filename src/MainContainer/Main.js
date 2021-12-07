a503571f564ffc5659ffae8e0ec4a55a0d9710da
Maedeh Hassani <maedeh.hassani@dawsoncollege.qc.ca>
2021-12-06 22:36:52 -0500

    finish the right bottom part

--- src/MainContainer/Main.js
+++ src/MainContainer/Main.js
@@ -7,7 +7,6 @@ import React, { useEffect, useState } from "react";
 
 const Main = () => {
     let [forumData, setFroumData] = useState(null);
-    let [post, setPost] = useState(null);
     //npx json-server --watch data/forum.json --port 3001
     console.log("npx json-server --watch data/forum.json --port 3001");
     useEffect(() => {
@@ -20,57 +19,61 @@ const Main = () => {
             }
         }).then(data => {
             setFroumData(data);
-            // showPost(data);
-
         }).catch(err => console.log(err.message));
     }, []);
-  
-//     useEffect(()=>{
-//     showPost(forumData);
-// })
-    function showPost(forumData) {
-        let topicName  = document.getElementById
-    }    
-    
-    
-  
+    // console.log(forumData[0].name);
+    let [forumList, setFroumList] = useState([
+        { "id": 1, "topic_id": "cat1_topic1_post1", "parentId": "topic1", "author": "nasr", "date": "2021-11-10 12:10", "text": "Are You Making These Coding using REactJS Mistakes?", "rate": 2.1, "likes": 0, "replies": 10 },
+        { "id": 2, "topic_id": "cat1_topic1_post1", "parentId": "topic1", "author": "badr", "date": "2021-11-14 12:30", "text": "This is a post TWO topic- SuperEasy Ways To Learn And Use REactJS", "rate": 2.1, "likes": 0, "replies": 5 },
+        { "id": 3, "topic_id": "cat1_topic1_post1", "parentId": "topic1", "author": "tricia", "date": "2021-10-16 12:30", "text": "This is a post 3- The Lazy Way To Coding using REactJS", "rate": 4.1, "likes": 22, "replies": 20 },
+        { "id": 4, "topic_id": "cat1_topic1_post1", "parentId": "topic1", "author": "dan", "date": "2018-12-09 11:30", "text": "This is a post 4 - Coding using REactJS Expert Interview", "rate": 3.1, "likes": 10, "replies": 15 },
+        { "id": 5, "topic_id": "cat1_topic1_post1", "parentId": "topic1", "author": "tricia", "date": "2021-05-11 12:30", "text": "This is a post 5-Brilliant Ways To Use Coding using REactJS", "rate": 4.1, "likes": 22, "replies": 20 }
+    ]);
+
     function handleForumdelete(frmId) {
         console.log("delete forum", frmId);
-        setFroumData(forumData.filter((frm) => {
+        setFroumList(forumList.filter((frm) => {
             return frm.id !== frmId;
         }));
     }
 
     function handleForumLike(frm, index) {
-        // console.log(posts);
         let likeId = document.getElementsByClassName("likesAndDislike");
         frm.likes = frm.likes + 1;
-        likeId[index].innerText = "Likes: " + `${frm.likes++}`;
+        likeId[index].innerText = `Likes: ${frm.likes++}`;
         frm.likes = frm.likes - 1;
     }
-    
+
     function handleForumdislike(frm, index) {
         let likeId = document.getElementsByClassName("likesAndDislike");
         frm.likes = frm.likes - 1;
-        likeId[index].innerText = "Likes: " + `${frm.likes--}`;
+        likeId[index].innerText = `Likes: ${frm.likes--}`;
         frm.likes = frm.likes + 1;
     }
-    
+    // function addOptions(options) {
+    //     console.log(options);
+    //     let selectList = document.getElementsByTagName('select');
+    //     for (let i = 0; i < selectList.length; i++) {
+    //         Object.keys(options).forEach((key) => {
+    //             let childNode = document.createElement('option');
+    //             childNode.innerText = `${options[key].name}`;
+    //             selectList[i].appendChild(childNode);
+    //         });
+    //     }
+    // }
     return (
         <section id="main-content">
             <LeftColumn />
             {forumData && <MainColum
                 forumData={forumData}
-                post={post}
-                setPost={setPost}
+                forumList={forumList}
                 handleForumdelete={handleForumdelete}
                 handleForumLike={handleForumLike}
                 handleForumdislike={handleForumdislike}
 
             />}
             <RightColumn
-                forumData={forumData}
-                
+                forumList={forumList}
             />
         </section>
     );
